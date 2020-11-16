c66c818298f4cbb1ac2ab62d279bffac27452f6e
MahmoudKhleif <mahmoud89ah@gmail.com>
2020-11-16 16:03:27 +0100

    initial commit

---
+++ Beräkningar/Class1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beräkningar
+{
+    class MatmatiskaBeräkningar
+    {   /*created a new class which contains 4 methods and these methods perform 
+        the Four arithmetic operations and then we can call methods when we need them 
+        
+        defined 5 variables at the class level as a static,
+        to be able to use it all over the class*/
+        static char w, e;
+        static double a, b, c;
+
+        // the first arithmetic operation (addiation)
+        public void Addition(char w, char e, int a, int b, int c)
+        {
+
+            if ((w == '+') && (e == '*'))
+            {
+                Console.WriteLine(a + " + " + b + " * " + c + " = " + (a + b * c));
+
+            }
+            else if ((w == '+') && (e == '-'))
+            {
+                Console.WriteLine(a + " + " + b + " - " + c + " = " + (a + b - c));
+            }
+            else if ((w == '+') && (e == '+'))
+            {
+                Console.WriteLine(a + " + " + b + " + " + c + " = " + (a + b + c));
+            }
+            else if ((w == '+') && (e == '/'))
+            {
+                Console.WriteLine(a + " + " + b + " / " + c + " = " + (a + b / c));
+            }
+
+        }
+        // the second arithmetic operation(Multiplikation)
+        public void Multiplikation(char w, char e, int a, int b, int c)
+        {
+            if ((w == '*') && (e == '*'))
+            {
+                Console.WriteLine(a + " * " + b + " * " + c + " = " + (a * b * c));
+
+            }
+            else if ((w == '*') && (e == '-'))
+            {
+                Console.WriteLine(a + " * " + b + " - " + c + " = " + (a * b - c));
+            }
+            else if ((w == '*') && (e == '+'))
+            {
+                Console.WriteLine(a + " * " + b + " + " + c + " = " + (a * b + c));
+            }
+            else if ((w == '*') && (e == '/'))
+            {
+                Console.WriteLine(a + " * " + b + " / " + c + " = " + (a * b / c));
+            }
+        }
+        // the third arithmetic operation(Subtraktion)
+        public void Subtraktion(char w, char e, int a, int b, int c)
+        {
+            if ((w == '-') && (e == '*'))
+            {
+                Console.WriteLine(a + " - " + b + " * " + c + " = " + (a - b * c));
+
+            }
+            else if ((w == '-') && (e == '-'))
+            {
+                Console.WriteLine(a + " - " + b + " - " + c + " = " + (a - b - c));
+            }
+            else if ((w == '-') && (e == '+'))
+            {
+                Console.WriteLine(a + " - " + b + " + " + c + " = " + (a - b + c));
+            }
+            else if ((w == '-') && (e == '/'))
+            {
+                Console.WriteLine(a + " - " + b + " / " + c + " = " + (a - b / c));
+            }
+
+        }
+        // the fourth arithmetic operation(Delning)
+        public void Delning(char w, char e, int a, int b, int c)
+        {
+            if ((w == '/') && (e == '*'))
+            {
+                Console.WriteLine(a + " / " + b + " * " + c + " = " + (a / b * c));
+
+            }
+            else if ((w == '/') && (e == '-'))
+            {
+                Console.WriteLine(a + " / " + b + " - " + c + " = " + (a / b - c));
+            }
+            else if ((w == '/') && (e == '+'))
+            {
+                Console.WriteLine(a + " / " + b + " + " + c + " = " + (a / b + c));
+            }
+            else if ((w == '/') && (e == '/'))
+            {
+                Console.WriteLine(a + " / " + b + " / " + c + " = " + (a / b / c));
+            }
+        }
+    }
+}
+  
---
+++ Beräkningar/Program.cs
@@ -0,0 +1,57 @@
+﻿using Beräkningar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beräkningar
+{
+    class Program
+    {   // defined 5 variables at the class level as a static,
+        // to be able to use it all over the class
+        static char w, e;
+        static int num1, num2, num3;
+       
+       //this method asks the user to enter 2 operators and 3 numbers.
+        static void Console1()
+        {
+            Console.WriteLine(" Enter first operator: ");
+            w = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(" Enter second operator: ");
+            e = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(" Enter first Number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Enter second Number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("  Enter third Number: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {   
+            MatmatiskaBeräkningar Beräkningar = new MatmatiskaBeräkningar();
+
+            string yes; // this variable for asking the user if wants to try again
+                        // and we use it in <<do- while>>> loop
+            do
+            {
+                Console1(); // call the method which ask user to input item
+
+                // call the methods which have the Four arithmetic operations
+                Beräkningar.Addition(w, e, num1, num2, num3);
+                Beräkningar.Multiplikation(w, e, num1, num2, num3);
+                Beräkningar.Subtraktion(w, e, num1, num2, num3);
+                Beräkningar.Delning(w, e, num1, num2, num3);
+
+          Console.WriteLine("Enter y if you want try again or any letter to Exit: ");
+           yes = Console.ReadLine();
+             } while (yes == "y" || yes == "Y"); // do-while loop to repeat code
+
+        }
+    }
+}
+
+
